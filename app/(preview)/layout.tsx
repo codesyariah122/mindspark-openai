3bef2c369788574964cfd3ee3ff528e7dcdb5c4a
codesyariah122 <pujiermanto@gmail.com>
2024-12-26 10:37:34 +0700

    update/new-edited

--- app/(preview)/layout.tsx
+++ app/(preview)/layout.tsx
@@ -5,8 +5,8 @@ import { AI } from "./actions";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://ai-sdk-preview-rsc-genui.vercel.dev"),
-  title: "Generative User Interfaces Preview",
-  description: "Generative UI with React Server Components and Vercel AI SDK",
+  title: "Mind Sparks | AI Project",
+  description: "Menyalakan Inovasi melalui Kecerdasan Buatan",
 };
 
 export default function RootLayout({
